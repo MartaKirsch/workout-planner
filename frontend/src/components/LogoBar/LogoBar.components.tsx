d4ebc7b8b6d1625fd39ff2a32b6cdca6dcabfdd9
Marta Kirsch <martakirsch00@gmail.com>
2021-10-12 14:56:02 +0200

    updated logo to redirect to home page

--- frontend/src/components/LogoBar/LogoBar.components.tsx
+++ frontend/src/components/LogoBar/LogoBar.components.tsx
@@ -7,9 +7,30 @@ export const LogoBarWrapper = styled.div`
   height: var(--logobar-height);
 
   padding: 0 40px;
+
+  a {
+    ${centeredLeftDiv};
+  }
 `;
 
 export const LogoBarText = styled.h2`
   font-size: ${({ theme }) => theme.fonts.sizes.xxs};
   color: ${({ theme }) => theme.fonts.colors.dark};
+
+  margin-left: 9px;
+
+  position: relative;
+
+  &::after {
+    content: "";
+
+    position: absolute;
+    bottom: -2px;
+    left: 0;
+
+    width: 100%;
+    height: ${({ theme }) => theme.borders.thiner};
+
+    background-color: ${({ theme }) => theme.colors.logoUnderline};
+  }
 `;
