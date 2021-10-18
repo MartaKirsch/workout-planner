da28fa03f109294115dca65ddda9d5d5d9771d30
Marta Kirsch <martakirsch00@gmail.com>
2021-10-18 11:01:24 +0200

    configured session store so it works, added register with save to db and set session

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma.service";
+import { CreateUserDto } from "./dto/createUser.dto";
 
 @Injectable()
 export class UserService {
-  getHello(): string {
-    return "Hello World!";
+  constructor(private prisma: PrismaService) {}
+  async register(userData: CreateUserDto) {
+    const user = await this.prisma.user.create({
+      data: {
+        name: userData.username,
+        email: userData.email,
+        password: userData.password,
+      },
+    });
+    return user;
   }
 }
