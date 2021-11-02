464f50c581a9f1daba6e73c27ab751da714cff1b
Marta Kirsch <martakirsch00@gmail.com>
2021-11-02 15:38:35 +0100

    modified global filter

--- backend/src/filters/global.filter.ts
+++ backend/src/filters/global.filter.ts
@@ -5,6 +5,7 @@ import {
   HttpException,
 } from "@nestjs/common";
 import { Response } from "express";
+import { CsrfException } from "src/exceptions/csrf.exception";
 
 @Catch(Error)
 export class GlobalFilter implements ExceptionFilter {
@@ -13,11 +14,14 @@ export class GlobalFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
 
     if (exception.code === "EBADCSRFTOKEN") {
-      response.status(404).json({ message: "Bad" });
+      // response.status(400).json({ message: "You have not been authenticated" });
+      response.status(400).json(new CsrfException().getResponse());
+      return;
     }
 
     if (exception instanceof HttpException) {
       response.status(404).json(exception.getResponse());
+      return;
     }
 
     response.status(400).json({ message: "Unhandled server error" });
