584b3b878f3441ddd951c0e7d0b9b256e559f486
Marta Kirsch <martakirsch00@gmail.com>
2021-11-17 15:51:27 +0100

    fixup! code refactor for exercises sidebar to be reused

--- frontend/src/components/shared/ExerciseContextWithSidebar/ExerciseContextWithSidebar.tsx
+++ frontend/src/components/shared/ExerciseContextWithSidebar/ExerciseContextWithSidebar.tsx
@@ -13,7 +13,7 @@ const ExerciseContextWithSidebar: FunctionComponent<Props> = ({
   return (
     <>
       <ExercisesContextProvider>
-        <ExerciseSidebar />
+        <ExerciseSidebar onTileClick={onTileClick} />
         {children}
       </ExercisesContextProvider>
     </>
